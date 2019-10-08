f23557e89bac7ede082c7a9cb26d4b96e49255ea
Rieder <47022821+riederleeDEV@users.noreply.github.com>
2019-10-08 18:29:22 +1100

    add rieder cnn code

--- cnn-code.py
+++ cnn-code.py
@@ -13,12 +13,14 @@ model.add(keras.layers.Conv2D(64, (3,3), activation='relu'))
 
 model.add(keras.layers.MaxPooling2D((2,2)))
 
+model.add(keras.layers.Conv2D(128, kernel_size=(3,3), 
+                              activation='relu', input_shape=input_shape))
+
+model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+
 model.add(keras.layers.Flatten())
 
 model.add(keras.layers.Dense(64, activation='relu'))
 
 model.add(keras.layers.Dense(10, activation='softmax'))
 
-model.add(keras.layers.Dense(128, activation='relu'))
-
-model.add(keras.layers.Dense(10, activation='softmax'))
