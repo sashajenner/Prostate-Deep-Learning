365a5ace11fed3e4fec3739b46abd45e4aa869fe
sashajenner <48614064+sashajenner@users.noreply.github.com>
2019-10-13 10:59:49 +1100

    Update slice_crop.py

--- main_code/slice_crop.py
+++ main_code/slice_crop.py
@@ -18,6 +18,10 @@ with open('../data/info.csv') as csv_file:
 
 lines.pop(0) # Removing the header
 
+'''
+Defining a function to read in an entry from 'info.csv',
+slice and crop the image with a `path_name`, saving it to its respective path
+'''
 def extract_image(entry, path_name, crop_height, crop_width):    
     ijk_in_crop = True
 
@@ -31,7 +35,7 @@ def extract_image(entry, path_name, crop_height, crop_width):
     img_file = nib.load('../data/mri/{}/{}.nii'.format(patient_id, path_name))
     image = img_file.get_fdata()
     image_sliced = image[:,:,k]
-
+    
     # Saving the sliced image
     save_path = '../data/mri/{}/{}_sliced_0{}.nii'.format(patient_id, path_name, fid)
     skimage.io.imsave(save_path, image_sliced)
@@ -44,7 +48,6 @@ def extract_image(entry, path_name, crop_height, crop_width):
         i = int(i * (384 / t2w_height))
         j = int(j * (384 / t2w_width))
 
-  #  print(i,j, image_h, image_w)
     # If the image height or width doesn't equal 384 scale it to that size
     if image_h != 384 or image_w != 384:
         image_sliced = cv2.resize(image_sliced, dsize = (384, 384), interpolation = cv2.INTER_NEAREST)
@@ -52,53 +55,39 @@ def extract_image(entry, path_name, crop_height, crop_width):
     h_mid = len(image_sliced) // 2
     w_mid = len(image_sliced[0]) // 2
     
-    #orig_height = crop_height
-    #orig_width = crop_width
-
-    #if patient_id == "pt_0181":
-        #crop_height = int(input("\ncrop height for pt_0181: ")) # 191    
-        #crop_width = int(input("crop width for pt_0181: ")) # 191
-     #   crop_height = 191
-      #  crop_width = 191
-    #elif patient_id == "pt_0075":
-        #crop_height = int(input("\ncrop height for pt_0075: ")) # 271
-        #crop_width = int(input("crop width for pt_0075: ")) # 271
-     #   crop_height = 271
-      #  crop_width = 271
-
     h = int((crop_height - 1) / 2)
     w = int((crop_width - 1) / 2)
 
     image_cropped = image_sliced[h_mid - h : h_mid + h + 1, w_mid - w: w_mid + w + 1]
     
-    # Scaling to input height and width
-    #if patient_id == "pt_0181" or patient_id == "pt_0075":
-     #   image_cropped = cv2.resize(image_cropped, dsize = (orig_width, orig_height), interpolation = cv2.INTER_NEAREST)
-
+    # If (i,j) not in the cropped image
     if not (i > h_mid - h and i < h_mid + h and j > w_mid - w and j < w_mid + w):
-#            or patient_id in ["pt_0181", "pt_0075"]:
-        print(i, j)
-        print(h_mid, h)
-        print(w_mid, w)
-    #    if patient_id not in ["pt_0181", "pt_0075"]:
         ijk_in_crop = False
-
+        
+        # Print a helpful error message
         print("ijk not in crop for:\npatient {},\nfid {}".format(patient_id, fid))
         
+        '''
+        Show on the left the image with the (i,j) and a box for where it is cropped,
+        and on the right the resulting cropped image for debugging
+        '''
+        # Creating an image with the (i,j) in white
         mask = image_sliced.copy()
         mask[:,:] = 0
         mask[i - 2 : i + 2, j - 2 : j + 2] = 255
+
+        # Plotting the sliced image with (i,j) and crop rectangle
         fig1, (ax1, ax2) = plt.subplots(1, 2)
         ax1.imshow(mask * 0.7 + image_sliced * 0.3, cm.gray)
         rect = patches.Rectangle((h_mid - h, w_mid - w), crop_height, crop_width, linewidth=1, edgecolor='r', facecolor='none')
         # Add the patch to the Axes
         ax1.add_patch(rect)
+        
+        # Plotting the cropped image in greyscale
         ax2.imshow(image_cropped, cm.gray)
+        
         plt.show()
         
-#    if patient_id in ["pt_0181", "pt_0075"] and input("good? ") == "no":
- #       return None
-
     # Saving cropped image
     save_path = '../data/mri/{}/{}_cropped_0{}.nii'.format(patient_id, path_name, fid)
     skimage.io.imsave(save_path, image_cropped)
@@ -107,29 +96,29 @@ def extract_image(entry, path_name, crop_height, crop_width):
     save_path = '../data/test/crop_{}/{}_{}.png'.format(path_name, patient_id, fid)
     skimage.io.imsave(save_path, image_cropped)
 
-    
-    return ijk_in_crop
+    return ijk_in_crop # Returning whether the (i,j) is in the cropped image
 
-
-#orig_height = int(input("crop height: ")) # 143
-#orig_width = int(input("crop width: ")) # 143
 orig_height = 143
 orig_width = 143
+
+if input("Would like to specify a crop size? ") == "yes":
+    orig_height = int(input("crop height: ")) # 143
+    orig_width = int(input("crop width: ")) # 143
 
 not_in_crop = 0
 
 index = 0
 while index < len(lines):
     entry = lines[index]
+
     for img_name in ["t2w", "adc", "ktrans"]:
         ijk_in_crop = extract_image(entry, img_name, orig_height, orig_width)
 
         if not ijk_in_crop:
             not_in_crop += 1     
     
-  #  if ijk_in_crop != None:
     index += 1
 
-    print("{}/326 done".format(index))
+    print("{}/326 done".format(index)) # Helpful printing message
 
 print(not_in_crop, "ijk's not making the crop")
