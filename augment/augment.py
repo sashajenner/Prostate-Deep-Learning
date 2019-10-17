422ebd18a87deb61670d4bd20ba23d26ab801905
sashajenner <48614064+sashajenner@users.noreply.github.com>
2019-10-17 14:45:23 +1100

    Move augmenting files

--- augment/augment.py
+++
@@ -1,8 +0,0 @@
-from keras.preprocessing.image import ImageDataGenerator
-
-path = "cropped_images"
-
-datagen = ImageDataGenerator(rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True)
-
-gen = datagen.flow_from_directory(path, target_size=(140,140), color_mode="rgba", batch_size=32)
-
