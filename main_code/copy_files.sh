8943c0c2901135ad16f334f4e50446e3393302d3
Sasha Jenner <48614064+sashajenner@users.noreply.github.com>
2020-07-22 15:14:21 +1000

    Delete copy_files.sh
    Ahh

--- main_code/copy_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-password="ExMachinaGen"
-
-sshpass -p $password scp -r sjen6644@headnode.physics.usyd.edu.au:~/data/models ../data
-sshpass -p $password scp -r sjen6644@headnode.physics.usyd.edu.au:~/main_code/models ./
-sshpass -p $password scp -r sjen6644@headnode.physics.usyd.edu.au:~/main_code/metrics.py ./
